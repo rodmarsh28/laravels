fb64181815a1f4df96572ca92f47106455fcf22e
rad091996 <101588926+rad091996@users.noreply.github.com>
2023-01-26 22:25:37 +0800

    aaa

---
+++ public/indexqq.php
@@ -0,0 +1,16 @@
+<script src="assets/js/loader.js"></script>
+<?php
+
+session_start();
+header("Location: ./checker.php");
+	// if(!empty($_SESSION["userId"])) {
+	// 	// $_POST["userid"]=$_SESSION["userId"];
+	// 	header("Location: ./checker.php");
+	// 	// echo "LOGIN SUCCESSFULLY !";
+	
+		
+	// }else{
+	// 	require_once __DIR__ . '/login.php';
+	// 	 } 
+//
+
